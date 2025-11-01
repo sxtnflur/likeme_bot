f9916462937efed74397fb02d497e3cfb7ca705b
Artyom <sheglove2003@bk.ru>
2025-11-01 23:34:57 +0700

    add remix_it_url

--- src/api/depends.py
+++ src/api/depends.py
@@ -4,14 +4,19 @@ from api.use_cases.fal_webhook import FalWebhookUseCase as FalWebhookUseCaseImpl
 from database.engine import get_db
 from depends import payments_service
 from fastapi import Depends
+from services.remixing import RemixingService as RemixingService_
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing_extensions import Annotated
 from depends import avatars_service
+from bot.loader import bot
 
 
 Db = Annotated[AsyncSession, Depends(get_db)]
 
+def get_remixing_service():
+    return RemixingService_(bot=bot)
 
+RemixingService = Annotated[RemixingService_, Depends(get_remixing_service)]
 
 
 def get_payments_use_case(
@@ -24,9 +29,9 @@ PaymentUseCase = Annotated[PaymentUseCaseImpl, Depends(get_payments_use_case)]
 
 
 def get_feed_use_case(
-    db: Db
+    db: Db, remixing_service: RemixingService
 ):
-    return FeedUseCaseImpl(db)
+    return FeedUseCaseImpl(db, remixing_service=remixing_service)
 
 
 FeedUseCase = Annotated[FeedUseCaseImpl, Depends(get_feed_use_case)]
